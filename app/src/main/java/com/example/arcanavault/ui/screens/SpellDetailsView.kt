35d1e2f05c6d9ca2a96e1d4758c78fcdf9858608
Adrian Macauley (s225733) <S225733@dtu.dk>
2025-01-09 17:14:53 +0100

    Removed unused imports

--- app/src/main/java/com/example/arcanavault/ui/screens/SpellDetailsView.kt
+++ app/src/main/java/com/example/arcanavault/ui/screens/SpellDetailsView.kt
@@ -1,9 +1,5 @@
 package com.example.arcanavault.ui.screens
 
-import android.R.attr.left
-import android.R.attr.right
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -21,7 +17,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.arcanavault.model.data.AreaOfEffect
