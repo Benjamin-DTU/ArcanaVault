ecef8e20658324401df12762dab768604f51a75c
Frederik <frederik.bode@hendrichsen.dk>
2025-01-15 10:45:54 +0100

    colors

--- app/src/main/java/com/example/arcanavault/ui/components/Header.kt
+++ app/src/main/java/com/example/arcanavault/ui/components/Header.kt
@@ -2,36 +2,41 @@ package com.example.arcanavault.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import com.example.arcanavault.ui.theme.ArcanaVaultTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Header(
     title: String,
     buttons: List<@Composable () -> Unit>,
-    scrollBehavior: TopAppBarScrollBehavior
+    scrollBehavior: TopAppBarScrollBehavior,
 ) {
-    CenterAlignedTopAppBar(
-        title = {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.titleLarge
-            )
-        },
-        actions = {
-            Row(
-                horizontalArrangement = Arrangement.End
-            ) {
-                buttons.forEach { button ->
-                    button()
+    ArcanaVaultTheme {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleLarge
+
+                )
+            },
+            actions = {
+                Row(
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    buttons.forEach { button ->
+                        button()
+                    }
                 }
-            }
-        },
-        scrollBehavior = scrollBehavior,
-        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-            containerColor = Color(0xFFF8E8EB)
+            },
+            scrollBehavior = scrollBehavior,
+            /*colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primary
+            )*/
         )
-    )
+    }
 }
