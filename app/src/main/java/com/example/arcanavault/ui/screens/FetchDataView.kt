8b9bd741bbb573613972d9d324fe0dd8450d27ae
Nicolai D. Madsen <s213364@student.dtu.dk>
2025-01-15 13:14:25 +0100

    Updated the FetchDataView.kt for a better display

--- app/src/main/java/com/example/arcanavault/ui/screens/FetchDataView.kt
+++ app/src/main/java/com/example/arcanavault/ui/screens/FetchDataView.kt
@@ -1,39 +1,34 @@
 package com.example.arcanavault.ui.components
 
-import androidx.compose.animation.core.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun FetchingDataView(
     modifier: Modifier = Modifier
 ) {
-    val infiniteTransition = rememberInfiniteTransition(label = "")
-    val dotsCount by infiniteTransition.animateValue(
-        initialValue = 1,
-        targetValue = 4,
-        typeConverter = Int.VectorConverter,
-        animationSpec = infiniteRepeatable(
-            animation = tween(500, easing = LinearEasing),
-            repeatMode = RepeatMode.Restart
-        ), label = ""
-    )
-
-    val dots = when (dotsCount) {
-        1 -> ".  "
-        2 -> ".. "
-        3 -> "..."
-        else -> "   "
-    }
-
     Box(
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "Fetching data$dots", fontSize = 36.sp)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier.size(50.dp), // Adjust the size of the progress indicator
+            )
+            Spacer(modifier = Modifier.height(16.dp)) // Adds spacing between the progress indicator and text
+            Text(
+                text = "Loading",
+                fontSize = 18.sp
+            )
+        }
     }
 }
