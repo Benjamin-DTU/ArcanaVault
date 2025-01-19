5e183e77d39ccb1e98a5855cc41b1ddc2ada09c6
sjbrandt <144696315+sjbrandt@users.noreply.github.com>
2025-01-19 13:10:55 +0100

    Add header to Favorites
    ... and remove back button.

--- app/src/main/java/com/example/arcanavault/ui/screens/FavoritesView.kt
+++ app/src/main/java/com/example/arcanavault/ui/screens/FavoritesView.kt
@@ -1,22 +1,25 @@
 package com.example.arcanavault.ui.screens
 
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBackIosNew
-import androidx.compose.material3.Icon
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import com.example.arcanavault.AppState
 import com.example.arcanavault.DB.FunctionsDB
 import com.example.arcanavault.model.data.Spell
+import com.example.arcanavault.ui.components.Header
 import com.example.arcanavault.ui.components.ListView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavouritesView(
     appState: AppState,
@@ -27,6 +30,8 @@ fun FavouritesView(
     val functionsDB = remember { FunctionsDB() }
     var favoriteSpells by remember { mutableStateOf(listOf<Spell>()) }
 
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    val scrollFraction = animateFloatAsState(targetValue = (scrollBehavior.state?.collapsedFraction ?: 0f))
 
     LaunchedEffect(Unit) {
         favoriteSpells = withContext(Dispatchers.IO) {
@@ -34,69 +39,60 @@ fun FavouritesView(
         }
     }
 
-    Column(modifier = modifier.fillMaxSize()) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Start
-        ) {
-            Icon(
-                imageVector = Icons.Filled.ArrowBackIosNew,
-                contentDescription = "Back",
-                modifier = Modifier
-                    .padding(8.dp)
-                    .height(24.dp)
-                    .padding(end = 8.dp)
-                    .clickable { onBackClick() }
-            )
-            Text(
-                text = "Back",
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier
-                    .padding(8.dp)
-                    .clickable { onBackClick() }
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            Header(
+                title = "Favorites",
+                buttons = listOf(),
+                scrollBehavior = scrollBehavior
             )
         }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Column(modifier = modifier.fillMaxSize()) {
+                if (favoriteSpells.isEmpty()) {
+                    Spacer(modifier = Modifier.height(48.dp))
+                    Text(
+                        text = "No favourites have been saved. " +
+                                "Click on the star icon to save a spell to your favourites.",
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                } else {
+                    ListView(
+                        items = favoriteSpells,
+                        onItemClick = { selectedSpell -> onSpellSelected(selectedSpell) },
+                        onFavoriteClick = { spell ->
 
-        Spacer(modifier = Modifier.height(16.dp))
+                            functionsDB.removeFromFavorites(spell.index)
 
-        if (favoriteSpells.isEmpty()) {
-            Spacer(modifier = Modifier.height(48.dp))
-            Text(
-                text = "No favourites have been saved. " +
-                        "Click on the star icon to save a spell to your favourites.",
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.padding(8.dp)
-            )
-        } else {
-            ListView(
-                items = favoriteSpells,
-                onItemClick = { selectedSpell -> onSpellSelected(selectedSpell) },
-                onFavoriteClick = { spell ->
+                            appState.updateSpellFavoriteStatus(spell.index, false)
 
-                    functionsDB.removeFromFavorites(spell.index)
+                            favoriteSpells = favoriteSpells.filter { it.index != spell.index }
 
-                    appState.updateSpellFavoriteStatus(spell.index, false)
-
-                    favoriteSpells = favoriteSpells.filter { it.index != spell.index }
-
-                    val updatedSpells = appState.getListOfSpells().map { s ->
-                        if (s.index == spell.index) {
-                            s.isFavorite = false
+                            val updatedSpells = appState.getListOfSpells().map { s ->
+                                if (s.index == spell.index) {
+                                    s.isFavorite = false
+                                }
+                                s
+                            }
+                            appState.setListOfSpells(updatedSpells)
+                        },
+                        titleProvider = { spell -> spell.name },
+                        detailsProvider = { spell ->
+                            listOf(
+                                "Level: ${spell.level}",
+                                "School: ${spell.school.name}"
+                            )
                         }
-                        s
-                    }
-                    appState.setListOfSpells(updatedSpells)
-                },
-                titleProvider = { spell -> spell.name },
-                detailsProvider = { spell ->
-                    listOf(
-                        "Level: ${spell.level}",
-                        "School: ${spell.school.name}"
                     )
                 }
-            )
-
-
+            }
         }
     }
 }
